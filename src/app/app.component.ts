7966988c658196782221e39cee34c77fc705950f
@belhassen_avec <belhassen.ben-ismail@avec.fr>
2022-12-11 22:40:12 +0100

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MessagingService } from './services/messaging.service';
 import { HttpClient ,HttpHeaders} from '@angular/common/http';
-import { SeoService } from './services/seo.service';
+//import { SeoService } from './services/seo.service';
 import { Meta, Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
@@ -11,7 +11,7 @@ import { Meta, Title } from '@angular/platform-browser';
 export class AppComponent {
   title = 'ihebProgectSOA';
   message:any
-  constructor(private  messagingService:MessagingService ,private http:HttpClient, private  seo:SeoService ,private meta:Meta){
+  constructor(private  messagingService:MessagingService ,private http:HttpClient,private meta:Meta){
 
   }
   ngOnInit() {
@@ -19,18 +19,18 @@ export class AppComponent {
       {
         name: 'keywords',
         content:'estate dubai ,real estate, vila dubai, real estate developers dubai, azizi,عقارات  دبي,منازل  في دبي, ابراج دبي' // Keywords
-    
+
       },
       { name: 'robots', content: 'index, follow' },
       { name: 'description', content: 'Pick your Property with heartofcarthage and enjoy with our best offer   Find your Perfect Property in dubai with us.  Luxuary Properties, Wonderful Views , the comfortable life in waiting for you ' },
-      
+
     ]);
     this.messagingService.requestPermission()
     this.messagingService.receiveMessage()
     this.message = this.messagingService.currentMessage
   //   const headers = new HttpHeaders({'Content-Type':'application/json','Authorization':"czEAEx7YWFnCC8Ei3wBBdv:APA91bEOyuQ0J4h2oD_OM2n292PsNI4bZX4pe1C2wTpg0ExGiecP3j"});
   //  var noti=  {notification: {
-  //   title: "Hey there", 
+  //   title: "Hey there",
   //   body: "Subscribe to mighty ghost hack youtube channel"
   //  },
   //  to : "uFvjswjLphkOzCEO0OcgM7yQGmzCZ7pKaFZ3iBhGJH4IxDlw9VIagvCVqFtFJ1a1esQqkKJG4jBtI58foa7CnB"
@@ -40,10 +40,10 @@ export class AppComponent {
     //     .post('https://fcm.googleapis.com/fcm/send/',noti, requestOptions )
     //     .subscribe(res => {
     //       console.log(res);
-    //     });  
-   ;  
+    //     });
+   ;
 
- 
+
 }
-  
+
 }
