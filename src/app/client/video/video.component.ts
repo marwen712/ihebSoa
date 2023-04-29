4cc6e08c25fe60f5e17b1af674488efb92b98e05
habib <ha9.0bib90@gmail.com>
2023-04-29 17:08:42 +0200

    'allah'

--- src/app/client/video/video.component.ts
+++ src/app/client/video/video.component.ts
@@ -31,31 +31,35 @@ scroll:any
     this.affBut=false
     }
   ngOnInit(): void {
+    console.log(this.route.snapshot.paramMap.get('id'),"id de page")
    this.crud.timeForm=20000
-    this.myVideo=document.getElementById("myVideo")
-    this.cot=document.getElementById("cot")
+    this.crud.id2=Number(this.crud.rev.find(ele=>ele.title == this.route.snapshot.paramMap.get('id')).id)
+  console.log(this.crud.rev.find(ele=>ele.title == this.route.snapshot.paramMap.get('id')),'id2')
+  this.titl.setTitle( this.crud.rev.find(ele=>ele.title == this.route.snapshot.paramMap.get('id')).title);
+  this.id=Number(this.crud.rev.find(ele=>ele.title == this.route.snapshot.paramMap.get('id')).id)
+this.crud.project=this.crud.rev[this.id].title
+this.video= this.sanitizer.bypassSecurityTrustResourceUrl(this.crud.rev[this.id].video);
+  // this.fun()
+ 
+  //  const videoPlayer = this.videoPlayerRef.nativeElement as HTMLVideoElement;
+    
+    // setTimeout(()=>{
+    //   //vid.contentWindow?.postMessage('play','*')
+    //   videoPlayer.load();
+    // },3000)
+//  this.crud.isCustomNavBar=false
+    // this.myVideo=document.getElementById("myVideo")
+    // this.cot=document.getElementById("cot")
   
-    this.cot.style.height=this.myVideo.style.height
+    // this.cot.style.height=this.myVideo.style.height
     this.render.listen(window, 'scroll', ($event) => {
       this.scroll = (window.scrollY / this.sections);
    console.log(this.scroll,"pp")
    
      
    })
-   console.log(this.route.snapshot.paramMap.get('id'))
-  this.crud.id2=Number(this.crud.rev.find(ele=>ele.title == this.route.snapshot.paramMap.get('id')).id)
-  this.titl.setTitle( this.crud.rev.find(ele=>ele.title == this.route.snapshot.paramMap.get('id')).title);
-  this.id=Number(this.crud.rev.find(ele=>ele.title == this.route.snapshot.paramMap.get('id')).id)
-this.crud.project=this.crud.rev[this.id].title
-  this.fun()
+   
  
-   const videoPlayer = this.videoPlayerRef.nativeElement as HTMLVideoElement;
-    
-    setTimeout(()=>{
-      //vid.contentWindow?.postMessage('play','*')
-      videoPlayer.load();
-    },3000)
- this.crud.isCustomNavBar=false
   //  var vid=this.myVideo as HTMLIFrameElement
   //  vid.contentWindow?.postMessage('play','*')
   // console.log(this.crud.rev[this.id].video)
