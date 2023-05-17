55d8bf0ceacf5698cada27c2128e88a1bb075d72
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2023-05-17 02:21:20 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,9 @@ testMaintenace = true
  video2:any
 //hubspot//
 changeInput1(e:any){
-if(e.target.value =="12"){
+
 this.testMaintenace=false
-}
+
 }
 ///
   constructor(private  messagingService:MessagingService ,private http:HttpClient,private meta:Meta ,public titl:Title, public crud:CrudService, private pixel: PixelService ){
