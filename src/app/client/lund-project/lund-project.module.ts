6659d6bcce6e96800b7227e3aed0348483b12fd8
habib <ha9.0bib90@gmail.com>
2023-12-01 22:08:19 +0100

    'llo'

--- src/app/client/lund-project/lund-project.module.ts
+++ src/app/client/lund-project/lund-project.module.ts
@@ -10,10 +10,7 @@ import { VideoComponent } from '../video/video.component';
 import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 
 
-import { CourbeCercleComponent } from 'src/app/azizi/courbe-cercle/courbe-cercle.component';
-import { AvantageComponent } from 'src/app/azizi/avantage/avantage.component';
-import { Avantage2Component } from 'src/app/azizi/avantage2/avantage2.component';
-import { HomeAziziComponent } from 'src/app/azizi/home-azizi/home-azizi.component'
+
 import {IvyCarouselModule} from 'angular-responsive-carousel';
 import { CarouselModule } from 'ngx-owl-carousel-o';;
 import { NgCircleProgressModule } from 'ng-circle-progress';
